344a401e7b00e9858f905f15ddaaeb3011adc8a8
Dandeib <97191252+Dandeib@users.noreply.github.com>
2024-02-07 13:44:17 +0100

    Update mongoConnect.tsx

--- utils/mongoConnect.tsx
+++ utils/mongoConnect.tsx
@@ -5,7 +5,7 @@ let cached: typeof mongoose | null = null;
 const mongoConnect = async (): Promise<typeof mongoose> => {
   if (cached) return cached;
   
-  const connection = await mongoose.connect("mongodb+srv://dandeib:de2542542@cluster0.nln9zsd.mongodb.net/");
+  const connection = await mongoose.connect("");
   cached = connection;
 
   console.log('connected to db');
